7a7de7054d15c582092579222b88ac40fe9c77e7
Mike Lam <mikelam231@gmail.com>
2014-08-26 11:56:48 -0700

    added UrbanDictionary checker

--- content.js
+++ content.js
@@ -13,12 +13,12 @@ var findMiddleLetter = function(string){
 		//experiment with only highlighting vowels
 		if(word.length %2 === 0){
 			var middleLetter = (string.length/2);
-			word.splice(middleLetter-1,0,'<b style="font-weight:bold">');
-			word.splice(middleLetter+1,0,'</b>');
+			word.splice(middleLetter-1,0,'<span style="font-weight: bold; color:#339933">');
+			word.splice(middleLetter+1,0,'</span>');
 		}else {
 			var middleLetter = Math.round(string.length/2);
-			word.splice(middleLetter-1,0,'<b style="font-weight:bold">');
-			word.splice(middleLetter+1,0,'</b>');
+			word.splice(middleLetter-1,0,'<span style="font-weight: bold; color:#339933">');
+			word.splice(middleLetter+1,0,'</span>');
 		}
 	}
 	var answer = word.join('');
@@ -39,20 +39,69 @@ var comicSansify = function(node){
 	node.css({"font-family":"Comic Sans MS"});
 }
 
+var checkUrbanForWord = function(config){
+	if(!config.ifFound) { throw new Error('needs an ifFound property'); }
+
+	var word = config['word'];
+	console.log('insideSpanWrap: '+word);
+	var onSuccess = function(data){
+		// console.log(word)
+		// varword = data.list[0].word;
+		if(data.result_type === 'exact'){
+			config.ifFound(word);
+		}
+	};
+	$.ajax({
+		url:"https://mashape-community-urban-dictionary.p.mashape.com/define?term="+word,
+		headers:{"X-Mashape-Key":"2g94OmfCpqmsh4KJHOCcXU4XwrI2p1ofUWnjsnV9AsUj3162Pj"}
+	}).done(onSuccess);
+}
+
+var urbanDic = function(node){
+	var string = node.text();
+	var sentence = node.text().split(' ');
+	console.log(sentence);
+	for(var i=0;i<sentence.length;i++){
+		(function(i){	
+			if(sentence[i].length>6){
+				checkUrbanForWord({
+					word: sentence[i],
+					ifFound: function(word) {
+						sentence[i] = '<span style="font-family: Comic Sans MS">'+word+'</span>';
+						node.html(sentence.join(' '));
+						console.log('sentence: '+sentence);
+					}
+				})
+				// console.log(sentence[i]);
+			}
+		})(i)
+	}
+	// console.log(sentence);
+	// node.html(sentence.join(' '));
+	
+}
+
 
 $(document).ready(function(){
+	var word = 'tomato'
+	$.ajax({
+		url:"https://mashape-community-urban-dictionary.p.mashape.com/define?term="+word,
+		headers:{"X-Mashape-Key":"2g94OmfCpqmsh4KJHOCcXU4XwrI2p1ofUWnjsnV9AsUj3162Pj"}
+	}).done(function(data){
+		// console.log(data.result_type==='exact');
+	})
 	var elements = $('html');
 	var recurse = function(node,callback){
 		if(node.children().length===0){
+			// console.log(node);
 			// node.html(roboEye(node.text())) -->this was before callbacks
 			callback(node);
 		} else {
 			for(var i = 0; i <node.children().length;i++){
-				console.log(node.children()[i])
 				recurse($(node.children()[i]),callback);
 			}
 		}
 	}
-	recurse($('html'),roboEye);
+	recurse($('html'),urbanDic);
 
 })
